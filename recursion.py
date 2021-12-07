1bee2b79b321b0cd887bc693edeb3e1a71bc518e
lalit-9238586578 <sahukumar220@gmail.com>
2021-12-07 15:38:36 +0530

    your commit message here

---
+++ recursion.py
@@ -0,0 +1,9 @@
+def fact(n):
+    if n == 1 :
+        print("factorial is ")
+
+
+print("enter a number to find its's factorial")
+n = input()
+result = fact(n)
+print("factorial of " n " is "result)
